f167c94689f18e7af6b5c2a0e0cd2253effad5ba
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2024-11-08 16:30:39 -0300

    Mudancas_Visuais
    mudanças visuais na tela de login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,60 +1,69 @@
-import React from 'react'
-import {FaUser, FaLock} from "react-icons/fa";
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { FaUser, FaLock } from "react-icons/fa";
 import "./login.css";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        alert("Dados de Login armazenados" + username );
+        if (rememberMe) {
+            alert("Dados de Login armazenados: " + username);
+        }
     };
 
-  return (
-    <div className="container">
-        <form onSubmit={handleSubmit}>
-            <h1>Are you ready?</h1>
-            <div className="input-field">
-                <FaUser className="icon" />
-                <input 
-                    type="email" 
-                    placeholder="E-mail" 
-                    onChange={(e) => setUsername (e.target.value)} 
-                />
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Are you ready?</h1>
                 
-            </div>
+                <div className="input-field">
+                    <FaUser className="icon" />
+                    <input 
+                        type="email" 
+                        placeholder="E-mail" 
+                        onFocus={(e) => e.target.placeholder = ""}
+                        onBlur={(e) => e.target.placeholder = "E-mail"}
+                        onChange={(e) => setUsername(e.target.value)} 
+                    />
+                </div>
 
-            <div className="input-field">
-                <FaLock className="icon" />
-                <input 
-                    type="password" 
-                    placeholder="Password"
-                    onChange={(e) => setPassword (e.target.value)} 
-                />
+                <div className="input-field">
+                    <FaLock className="icon" />
+                    <input 
+                        type="password" 
+                        placeholder="Password"
+                        onFocus={(e) => e.target.placeholder = ""}
+                        onBlur={(e) => e.target.placeholder = "Password"}
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                </div>
 
-            </div>
-
-            <div className="recall-forget">
-                <label>
-                    <input type="checkbox"/>
-                    Remember me 
-                </label>       
-                   <a href="#">Forgot your password?</a>   
-            </div>            
-            
-            <button>Run</button>
-           
-            <div className="signup-link">
-                <p>
-                Don't have an account? <a href="#">Register new account</a>
-                </p>
-            </div>
-        </form>     
-    </div>
-  )
+                <div className="recall-forget">
+                    <label>
+                        <input 
+                            type="checkbox"
+                            checked={rememberMe}
+                            onChange={(e) => setRememberMe(e.target.checked)} 
+                        />
+                        Remember me 
+                    </label>       
+                    <a href="#">Forgot your password?</a>   
+                </div>            
+                
+                <button type="submit">Run</button>
+               
+                <div className="signup-link">
+                    <p>
+                        Don't have an account? <a href="#">Register new account</a>
+                    </p>
+                </div>
+            </form>     
+        </div>
+    );
 }
 
-export default Login
+export default Login;
